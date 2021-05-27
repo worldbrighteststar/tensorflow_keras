968f1360cc93f8ab4d1b16f848abc2b97dfeca45
choi <scott.choi1993@gmail.com>
2021-05-27 20:01:07 +0900

    metrics option

--- keras/keras06_mlp3.py
+++ keras/keras06_mlp3.py
@@ -17,11 +17,18 @@ model.add(Dense(20))
 model.add(Dense(1))
 
 # 3. compile and train
-model.compile(loss='mse',
-              optimizer='adam',
-              metrics=['mae']) # metrics : 번외로 다른 지표를 훈련 동안 볼 수 있음
+model.compile(loss='mse', 
+              optimizer='adam', 
+              metrics=['mae', 'acc']) # metrics : 번외로 다른 지표를 훈련 동안 볼 수 있음
 model.fit(x, y, epochs=200, batch_size=1)
 
 # 4. evaluate
 loss = model.evaluate(x, y) # metrics에 명시된 지표도 evaluate에 나타난다.
-print(f'loss : {loss}')
+print(f'loss(mse) : {loss[0]}, mae : {loss[1]}, acc : {loss[2]}')
+
+"""
+회귀 vs 분류
+회귀는 오차의 거리를,
+분류는 선택지가 맞는지 틀린지를 판단
+=> accuracy는 분류에서 사용하는 판단 지표이다.
+"""
