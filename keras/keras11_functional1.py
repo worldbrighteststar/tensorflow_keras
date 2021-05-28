d6f528595c8f05b5d216ddc88fba46131408218d
choi <scott.choi1993@gmail.com>
2021-05-28 21:43:18 +0900

    functional api modeling

--- keras/keras11_functional1.py
+++ keras/keras11_functional1.py
@@ -19,25 +19,25 @@ x_train, x_test, y_train, y_test = train_test_split(
 from tensorflow.keras.models import Sequential, Model # add Model
 from tensorflow.keras.layers import Dense, Input # add Input
 
-"""
+
 # 함수형 모델은 input에 대한 명시가 따로 필요.
 input1 = Input(shape=(5,))
-dense1 = Dense(3)(input1)
-dense2 = Dense(4)(dense1)
+dense1 = Dense(20)(input1)
+dense2 = Dense(10)(dense1)
 output1 = Dense(2)(dense2)
 model = Model(inputs=input1, outputs=output1) # 함수형 모델은 마지막에 모델 명시(input layer, output layer)
 """
 # IF Sequential model시
 model = Sequential()
-model.add(Dense(10, input_shape=(5,)))
-model.add(Dense(5))
+model.add(Dense(20, input_shape=(5,)))
+model.add(Dense(10))
 model.add(Dense(2))
-
+"""
 model.summary() # 모델의 구조를 볼 수 있다.
 
 # 3. complie and train
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=100)
+model.fit(x_train, y_train, epochs=200, batch_size=20)
 
 # 4. evaluate and predict
 loss = model.evaluate(x_test, y_test)
